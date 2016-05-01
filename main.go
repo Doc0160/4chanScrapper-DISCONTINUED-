fd5bb794386e083eded9c3711c62424c9c228501
Doc0160 <t0160d@gmail.com>
2016-05-02 00:25:52 +0200

    check for errors while downloading

--- main.go
+++ main.go
@@ -33,61 +33,73 @@ func main(){
 	go func(){
 		for true{
 			file := <-dl
-			os.MkdirAll("./"+file.Folder+"/", 0777)
-			t := "./"+file.Folder+"/"+file.Filename
-			if _, err := os.Stat(t); os.IsNotExist(err){
-				fmt.Println(file.Filename, "downloading")
-				out, err := os.Create(t)
-				if err==nil{
-					resp, err := http.Get(file.Url)
-					if err!=nil{
-						out.Close()
-						os.Remove(t)
-					}else{
-						io.Copy(out, resp.Body)	
-						out.Close()
-						resp.Body.Close()
-						fmt.Println(file.Filename, "downloaded")
-						// check duplicates and keep the most recent one
-						go func(){
-							f1,_ := os.Stat(t)
-							files, _ := ioutil.ReadDir("./"+file.Folder)
-							for _, f2 := range files{
-								// big deep check byte per byte if necessary to determine if two files are the same
-								if f1.Name()!=f2.Name() && func()bool{
-									if f1.Size()!=f2.Size(){
-										return false
-									}
-									sf, err := os.Open("./"+file.Folder+"/"+f1.Name())
-									defer sf.Close()
-									if err != nil {
-										return false
-									}
-									df, err := os.Open("./"+file.Folder+"/"+f2.Name())
-									defer df.Close()
-									if err != nil {
-										return false
-									}
-									sscan := bufio.NewScanner(sf)
-									dscan := bufio.NewScanner(df)
-									for sscan.Scan() {
-										dscan.Scan()
-										if !bytes.Equal(sscan.Bytes(), dscan.Bytes()) {
-											return false
+			if strings.Contains(file.Filename,"."){
+				os.MkdirAll("./"+file.Folder+"/", 0777)
+				t := "./"+file.Folder+"/"+file.Filename
+				if _, err := os.Stat(t); os.IsNotExist(err){
+					fmt.Println(file.Filename, "downloading")
+					out, err := os.Create(t)
+					if err==nil{
+						resp, err := http.Get(file.Url)
+						if err!=nil{
+							fmt.Println(err)
+							out.Close()
+							os.Remove(t)
+						}else{
+							_, err := io.Copy(out, resp.Body)	
+							out.Close()
+							resp.Body.Close()
+							if err==nil{
+								fmt.Println(file.Filename, "downloaded")
+								// check duplicates and keep the most recent one
+								go func(){
+									f1,_ := os.Stat(t)
+									files, _ := ioutil.ReadDir("./"+file.Folder)
+									for _, f2 := range files{
+										// big deep check byte per byte if necessary to determine if two files are the same
+										if f1.Name()!=f2.Name() && func()bool{
+											if f1.Size()!=f2.Size(){
+												return false
+											}
+											sf, err := os.Open("./"+file.Folder+"/"+f1.Name())
+											defer sf.Close()
+											if err != nil {
+												fmt.Println(err)
+												return false
+											}
+											df, err := os.Open("./"+file.Folder+"/"+f2.Name())
+											defer df.Close()
+											if err != nil {
+												fmt.Println(err)
+												return false
+											}
+											sscan := bufio.NewScanner(sf)
+											dscan := bufio.NewScanner(df)
+											for sscan.Scan() {
+												dscan.Scan()
+												if !bytes.Equal(sscan.Bytes(), dscan.Bytes()) {
+													return false
+												}
+											}
+											return true
+										}(){
+											if f1.ModTime().Before(f2.ModTime()){
+												fmt.Println(f2.Name(), "duplicate removed")
+												os.Remove("./"+file.Folder+"/"+f1.Name())
+											}else{
+												fmt.Println(f1.Name(), "duplicate removed")
+												os.Remove("./"+file.Folder+"/"+f2.Name())
+											}
 										}
 									}
-									return true
-								}(){
-									if f1.ModTime().Before(f2.ModTime()){
-										fmt.Println(f2.Name(), "duplicate removed")
-										os.Remove("./"+file.Folder+"/"+f1.Name())
-									}else{
-										fmt.Println(f1.Name(), "duplicate removed")
-										os.Remove("./"+file.Folder+"/"+f2.Name())
-									}
-								}
+								}()
+							}else{ // if error on download, retry later
+								os.Remove(t)
+								dl<-file
 							}
-						}()
+						}
+					}else{
+						fmt.Println(err)
 					}
 				}
 			}
